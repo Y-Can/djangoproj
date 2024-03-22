e5e48bcbb75075349d51190b41547df2d1036146
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-23 00:07:13 +0100

    fix

--- djangoproj/settings.py
+++ djangoproj/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-ALLOWED_HOSTS=['.vercel.app', '127.0.0.1', 'localhost', 'https://djangoproj.onrender.com/']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,8 @@ SECRET_KEY = 'django-insecure-ab=716hx$i^8dfe@ccn6yzt3e03^x%c_#-&a=k5wba92res@#v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['.vercel.app', '127.0.0.1', 'localhost', 'https://djangoproj.onrender.com/']
+
 
 
 # Application definition
