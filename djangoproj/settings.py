2d034d4fba2dc7f8112fab498c8f7ea23a28defd
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-22 23:58:08 +0100

    fiox depolor

--- djangoproj/settings.py
+++ djangoproj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-ALLOWED_HOSTS=['.vercel.app']
+ALLOWED_HOSTS=['.vercel.app', '127.0.0.1', 'localhost', 'https://djangoproj.onrender.com/']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
